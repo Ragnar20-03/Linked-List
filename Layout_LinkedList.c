46b19007d51667731edbfe32155f17f8bede99b7
Ragnar20-03 <roshanpp20@gmail.com>
2023-06-13 14:36:15 +0530

    INsert First , Last

--- Layout_LinkedList.c
+++ Layout_LinkedList.c
@@ -1,34 +1,99 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct Node {
+ struct Node{
     int data;
-    struct Node * next ;
+    struct Node * next;
 };
 
 typedef struct Node NODE;
-typedef struct Node * PNODE;
-typedef struct Node **PPNODE;
+typedef struct Node * PNODE ; 
+typedef struct Node ** PPNODE;
 
 
-void InsertFirst(PPNODE Head , int No)
+void InsertLast( PPNODE Head , int iNo)
 {
-
+    PNODE temp = *Head;
     PNODE newn = NULL;
-    newn = (PNODE) malloc(sizeof(NODE)) ;
+    newn = (PNODE) malloc(sizeof(NODE));
 
-    newn-> data = No;
-    newn-> next = NULL;
+    newn -> data = iNo;
+    newn -> next = NULL;
+
+    if(*Head == NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        while (temp -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+        temp -> next = newn;
+    }
+}
+
+void InsertFirst( PPNODE Head  , int iNo)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE) malloc (sizeof(NODE));
+    newn -> data = iNo;
+    newn -> next = NULL;
 
     if( *Head == NULL)
     {
         *Head = newn;
     }
+    else
+    {
+        newn -> next = *Head;
+        *Head = newn;
+    }
+
+}
+
+void Display ( PNODE Head)
+{
+    printf("Linked list is :\n");
+    while (Head  != NULL)
+    {
+        printf("%d\t",Head->data);
+        Head = Head -> next;
+    }
+    printf("\n");
+}
+
+void Count ( PNODE Head)
+{
+    int iCnt = 0;
+    printf("CCount of Linked list is :\n");
+    while (Head  != NULL)
+    {
+        iCnt++;
+        Head = Head -> next;
+    }
+    printf("%d \n" , iCnt);
 }
 
 int main()
 {
-    PNODE First = NULL;
+    PNODE First = NULL;  // Main Pointer which points to Linked List;
+
+    InsertFirst( &First , 51);
+    InsertFirst( &First , 11);
+    InsertFirst( &First , 21);
+
+    Display(First);
+
+    printf("After insert Last \n");
+
+    InsertLast(&First , 511);
+    InsertLast(&First , 522);
+
+    Display(First);
+    Count(First);
 
     return 0;
 }
