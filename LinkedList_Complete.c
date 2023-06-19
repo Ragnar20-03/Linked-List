0a69d340a6abc4edb4210057da06c33757405106
Ragnar20-03 <roshanpp20@gmail.com>
2023-06-19 17:48:29 +0530

    Linked List

--- LinkedList_Complete.c
+++ LinkedList_Complete.c
@@ -1,18 +1,52 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct Node {
+typedef struct Node
+{
     int data ;
-    struct Node * next ;
-};
+    struct Node * next;
+}NODE , * PNODE , ** PPNODE;
 
-typedef struct Node NODE;
-typedef struct Node * PNODE;
-typedef struct Node ** PPNODE;
+int Count ( PNODE Head )
+{
+    int iCount = 0;
+    while ( Head != NULL)
+    {
+        iCount ++;
+        Head = Head -> next;
+    }
+    return iCount;
+}
+void Display( PNODE Head)
+{
+    printf("Linked List is : \n\n");
+    while ( Head != NULL)
+    {
+        printf("%d : ", Head -> data);
+        Head = Head -> next;
+    }
+    printf("\n");
+}
+
 
 void InsertFirst ( PPNODE Head , int No)
 {
-    PNODE newn = (PNODE)malloc ( sizeof(NODE));
+    PNODE newn = (PNODE)malloc(sizeof(NODE));
+        newn -> data = No;
+        newn -> next = NULL;
+    if(*Head == NULL)
+    {
+        *Head = newn;
+    }
+    else 
+    {
+        newn -> next = *Head;
+        *Head = newn;
+    }
+}
+void InsertLast ( PPNODE Head , int No)
+{
+    PNODE newn = (PNODE) malloc (sizeof (NODE));
         newn -> next = NULL;
         newn -> data = No;
     
@@ -22,19 +56,55 @@ void InsertFirst ( PPNODE Head , int No)
     }
     else
     {
-        newn -> next = *Head ;
-        *Head = newn;
+        PNODE temp = *Head ; 
+        while (temp -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+        temp -> next = newn;
     }
 }
 
-void DeleteFirst ( PPNODE Head )
-{   
+void InsertAtPos ( PPNODE Head , int No , int Pos)
+{
+    int iLength = Count ( *Head );
+        if (( Pos < 0 ) || (Pos > iLength + 1) )
+        {
+            printf("Invalid Position .. \n");
+            return;
+        }  
+    if ( Pos == 1)
+    {
+        InsertFirst ( Head , No);
+    }    
+    else if ( Pos == iLength + 1)
+    {
+        InsertLast ( Head , No );
+    }
+    else
+    {
+        PNODE newn = (PNODE) malloc (sizeof(NODE));
+            newn -> next = NULL;
+            newn -> data = No;
+
+        PNODE temp = *Head ;
+            for ( int iCnt = 1 ; iCnt < Pos - 1 ; iCnt ++)
+            {
+                temp = temp -> next;
+            }
+        newn -> next = temp -> next;
+        temp -> next = newn;
+        
+    }
+}
+
+void DeleteFirst ( PPNODE Head)
+{
     if ( *Head == NULL)
     {
-        printf(" Linkde List is Empty .. \n");
         return;
     }
-    else if ( (*Head )-> next == NULL)
+    else if ( (*Head) -> next == NULL)
     {
         free ( *Head );
         *Head = NULL;
@@ -42,19 +112,18 @@ void DeleteFirst ( PPNODE Head )
     else
     {
         PNODE temp = *Head ; 
-        *Head = (*Head )-> next;
-        free (temp);
+        *Head = (*Head) -> next;
+        free ( temp);
     }
 }
 
-void DeleteLast ( PPNODE Head )
+void DeleteLast( PPNODE Head )
 {
     if ( *Head == NULL)
     {
-        printf("Linked List is Empty ..\n");
-        return ;
+        return;
     }
-    else if ( (*Head) -> next == NULL)
+    else if ((*Head) -> next == NULL)
     {
         free ( *Head );
         *Head = NULL;
@@ -66,8 +135,38 @@ void DeleteLast ( PPNODE Head )
         {
             temp = temp -> next;
         }
-        free(temp -> next );
+        free ( temp -> next);
         temp -> next = NULL;
+    }
+}
+
+void DeleteAtPos ( PPNODE Head , int Pos)
+{
+    int iLength = Count ( *Head );
+        if (( Pos < 1) || (Pos > iLength) )
+        {
+            printf ( "Invalid Position .. \n");
+            return;
+        }
+    if ( Pos == 1)
+    {
+        DeleteFirst ( Head );
+    }
+    else if ( Pos == iLength)
+    {
+        DeleteLast ( Head );
+    }
+    else 
+    {
+        PNODE temp = *Head ; 
+         for ( int iCnt = 1 ; iCnt < Pos - 1 ; iCnt++)
+         {
+            temp = temp -> next;
+         }
+        PNODE tempX = temp -> next; 
+        temp -> next = temp -> next -> next;
+        free ( tempX);
+
     }
 }
 
@@ -75,5 +174,22 @@ int main()
 {
     PNODE First = NULL;
 
+        InsertAtPos ( &First  , 11 , 1 ); 
+        InsertAtPos ( &First , 21 , 1 ); 
+        InsertAtPos ( &First , 41 , 1 ); 
+        InsertAtPos ( &First , 51 , 1 ); 
+        InsertAtPos ( &First , 101 , 1 ); 
+        InsertAtPos ( &First , 111 , 1 ); 
+
+        Display ( First );
+
+        DeleteAtPos ( &First , 1);
+        Display ( First );
+        DeleteAtPos ( &First , 5);
+        Display ( First );
+        DeleteAtPos ( &First , 3);
+
+        Display ( First);
+
     return 0;
-}
+}   
