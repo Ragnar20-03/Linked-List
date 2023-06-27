7587e68a42ad6bb6c2abefd90bb7ca41a99847a7
Ragnar20-03 <roshanpp20@gmail.com>
2023-06-28 00:06:54 +0530

    DL Linked List

--- Demo.c
+++ Demo.c
@@ -4,206 +4,25 @@
 
 struct Node 
 {
-    int data ;
-    struct Node * next;
+    struct Node * prev ; 
+        int data ;
+    struct Node * next ;
 };
-typedef struct Node  NODE;
+
+typedef struct Node NODE;
 typedef struct Node * PNODE;
 typedef struct Node ** PPNODE;
 
 
-void InsertFirst ( PPNODE Head , int No)
+void InsertFirst ( PPNODE Head , PPNODE Tail , int No)
 {
-    PNODE newn  = (PNODE) malloc ( sizeof (NODE));
-        newn -> data = No;
-        newn -> next = NULL;
     
-    if ( *Head == NULL)
-    {
-        *Head = newn ;
-    }
-    else
-    {
-        newn -> next = *Head ;
-        *Head = newn ;
-    }
 }
 
-void InsertLast ( PPNODE Head , int No)
-{
-    PNODE newn  =  (PNODE) malloc (sizeof ( NODE));
-        newn -> data = No;
-        newn ->  next = NULL;
-    if ( *Head == NULL)
-    {
-        *Head = newn ;
-    }
-    else
-    {
-        PNODE temp = *Head;
-        while ( temp -> next != NULL)
-        {
-            temp = temp -> next;
-        }    
-        temp -> next = newn;
-    }
-}
-
-void DeleteFirst ( PPNODE Head )
-{
-    if ( *Head == NULL)
-    {
-        return ;
-    }
-    else if ( (*Head) -> next == NULL)
-    {
-        free ( *Head);
-        *Head = NULL;
-    }
-    else 
-    {
-        PNODE temp = *Head;
-        *Head = (*Head) -> next;
-        free(temp);
-    }
-}
-
-void DeleteLast ( PPNODE Head )
-{
-    if ( *Head == NULL)
-    {
-        return;
-    }
-    else if  ( (*Head) -> next == NULL)
-    {
-        free ( *Head );
-        *Head = NULL;
-    }
-    else 
-    {
-        PNODE temp = *Head;
-        while ( temp -> next -> next != NULL )
-        {
-            temp = temp -> next;
-        }
-        free ( temp -> next);
-        temp -> next = NULL;
-    }
-}
-
-int  Display ( PNODE Head )
-{
-    int iCount = 0;
-    printf ( "Elemenys of Linked List  are : \n "  );
-    while ( Head  != NULL)
-    {
-        printf(" %d : " , Head -> data);
-        Head = Head -> next;
-        iCount ++;
-    }
-    printf("\n Count is : : %d \n" ,iCount);
-    return iCount;
-}
-
-void InsertAtPos ( PPNODE Head , int No , int iPos)
-{
-    int iLength = Display ( *Head );
-        if (( iPos < 1) || ( iPos > iLength + 1) )
-        {
-            return ;
-        }
-    if ( iPos == 1)
-    {
-        InsertFirst ( Head, No);
-    }
-    else if ( iPos == iLength + 1)
-    {
-        InsertLast ( Head , No);
-    }
-    else 
-    {
-        PNODE temp = *Head;
-        PNODE newn =  (PNODE) malloc ( sizeof ( NODE));
-            newn -> next = NULL;
-            newn -> data = No;
-
-        for ( int iCnt = 1; iCnt < iPos -1 ; iCnt ++)
-        {
-            temp = temp -> next;
-        }
-        newn -> next = temp -> next;
-        temp -> next = newn ;
-    }
-}
-
-void DeleteAtPos (PPNODE Head , int iPos)
-{
-    int iLength = Display ( *Head );
-        if ((iPos < 1 )  || ( iPos > iLength)  )
-        {
-            return ;
-        }
-    if ( iPos == 1)
-    {
-        DeleteFirst ( Head);
-    }
-    else if ( iPos == iLength)
-    {
-        DeleteLast ( Head);
-    }
-    else 
-    {
-        PNODE temp = *Head ; 
-        for ( int iCnt = 1; iCnt < iPos -1 ; iCnt ++)
-        {
-            temp = temp -> next;
-        }
-        PNODE tempX = temp -> next;
-        temp -> next = temp -> next -> next;
-        free ( tempX);
-    }
-}
-
-
-int main ()
+int main()
 {
     PNODE First = NULL;
-        int iRet = 0;
-
-    InsertLast(&First, 11);
-    InsertLast(&First, 21);
-    InsertLast(&First, 51);
-    InsertLast(&First, 101);
-
-    Display(First);
-
-
-    InsertFirst(&First,10);
-    InsertFirst(&First,20);
-
-    InsertAtPos(&First, 25, 5);
-
-    Display(First);
-
-
-    
-    DeleteAtPos(&First, 5);
-
-    Display(First);
-
-
-    
-    DeleteFirst(&First);
-    DeleteFirst(&First);
-
-    Display(First);
-
-
-
-    DeleteLast(&First);
-    Display(First);
-
-
+    PNODE Last  = NULL;
 
     return 0;
 }
